cb79e37f2e914bcd54f811b1610dd2d5cbd8825b
João Ferreira Loff <jfloff@gmail.com>
2021-08-26 16:42:36 +0000

    feat: Update templates for mysql-sns deployment

--- mysql-sns/reader/reader.py
+++ mysql-sns/reader/reader.py
@@ -1,10 +1,10 @@
 import json
 import os
-import boto3
 import pymysql
 import pymysql.cursors
 from pprint import pprint
 from datetime import datetime
+import boto3
 import time
 
 
@@ -19,6 +19,13 @@ import time
 #   - do not forget to invoke the writer first
 # { "i": "1", "key": "AABB11", "written_at": "2020-07-13 10:44:29.767250" }
 #---------------
+
+MYSQL_HOST = os.environ.get('MYSQL_HOST')
+MYSQL_PORT = int(os.environ.get('MYSQL_PORT'))
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_DB = os.environ.get('MYSQL_DB')
+MYSQL_POST_TABLE_NAME = os.environ.get('MYSQL_POST_TABLE_NAME')
 
 def lambda_handler(event, context):
   # if we have an event from SNS topic we parse it
@@ -37,28 +44,27 @@ def lambda_handler(event, context):
     'ts_read_post_blob_spent': None,
   }
 
+  # connect to mysql
   while True:
     try:
-      mysql_conn = pymysql.connect('antipode-lambda-global-cluster-1.cluster-ro-cdwqaw2esdz0.us-east-1.rds.amazonaws.com',
-          port = 3306,
-          user='antipode',
-          password='antipode',
-          connect_timeout=5,
-          db='antipode',
-          autocommit=True
-        )
+      mysql_conn = pymysql.connect(
+        host=MYSQL_HOST,
+        port=MYSQL_PORT,
+        user=MYSQL_USER,
+        password=MYSQL_PASSWORD,
+        db=MYSQL_DB,
+        connect_timeout=30,
+        autocommit=True
+      )
       break
     except pymysql.Error as e:
       print(f"[ERROR] MySQL exception: {e}")
 
   # read post
-  print("[INFO] Reading post", end='')
-
   ts_read_post_key_start = datetime.now()
   while True:
     with mysql_conn.cursor() as cursor:
-      print('', end ='...')
-      sql = "SELECT `k` FROM `blobs` WHERE `v`=%s"
+      sql = f"SELECT `k` FROM `{MYSQL_POST_TABLE_NAME}` WHERE `v`=%s"
       cursor.execute(sql, (event['key'],))
       result = cursor.fetchone()
 
@@ -74,8 +80,7 @@ def lambda_handler(event, context):
   ts_read_post_blob_start = datetime.now()
   while True:
     with mysql_conn.cursor() as cursor:
-      print('', end ='...')
-      sql = "SELECT `b` FROM `blobs` WHERE `v`=%s"
+      sql = f"SELECT `b` FROM `{MYSQL_POST_TABLE_NAME}` WHERE `v`=%s"
       cursor.execute(sql, (event['key'],))
       result = cursor.fetchone()
 
@@ -83,19 +88,14 @@ def lambda_handler(event, context):
       if result is None:
         evaluation['read_post_retries'] += 1
         evaluation['read_post_blob_retries'] += 1
-        print("\t Retry read post...")
       else:
         evaluation['ts_read_post_blob_spent'] = (datetime.now() - ts_read_post_blob_start).total_seconds()
         evaluation['ts_read_post_spent'] = (datetime.now() - ts_read_post_key_start).total_seconds()
         break
 
-  print("[INFO] Done!")
-
   # write evaluation to dynamo
-  # print("[INFO] Writing eval to dynamo...", end='')
   # table_conn = boto3.resource('dynamodb', region_name='eu-central-1').Table("antipode-eval")
   # table_conn.put_item(Item=evaluation)
-  # print("[INFO] Done!")
 
   return {
     'statusCode': 200,
---
+++ mysql-sns/writer/writer.py
@@ -0,0 +1,66 @@
+import json
+import os
+import boto3
+import pymysql
+import pymysql.cursors
+from pprint import pprint
+import time
+from datetime import datetime
+
+#---------------
+# AWS SAM Deployment details
+#
+# Region: eu-central-1 (Frankfurt)
+# S3 Bucket: antipode-lambda-eu
+# Stack name: mysql-sns
+#
+# Payload Example:
+# { "i": "1", "key": "AABB11" }
+#---------------
+
+MYSQL_HOST = os.environ.get('MYSQL_HOST')
+MYSQL_PORT = int(os.environ.get('MYSQL_PORT'))
+MYSQL_USER = os.environ.get('MYSQL_USER')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
+MYSQL_DB = os.environ.get('MYSQL_DB')
+MYSQL_POST_TABLE_NAME = os.environ.get('MYSQL_POST_TABLE_NAME')
+SNS_ARN = os.environ.get('SNS_ARN')
+
+def lambda_handler(event, context):
+  try:
+    # connect to mysql
+    mysql_conn = pymysql.connect(
+      host=MYSQL_HOST,
+      port=MYSQL_PORT,
+      user=MYSQL_USER,
+      password=MYSQL_PASSWORD,
+      db=MYSQL_DB,
+      connect_timeout=30,
+      autocommit=True
+    )
+
+    # write post
+    with mysql_conn.cursor() as cursor:
+      # write with 0:AAAA -> blob of 1Mb
+      # 1MB is the maximum packet size!!
+      sql = f"INSERT INTO `{MYSQL_POST_TABLE_NAME}` (`k`, `v`, `b`) VALUES (%s, %s, %s)"
+      cursor.execute(sql, (int(event['i']), event['key'], os.urandom(1000000)))
+      mysql_conn.commit()
+
+    # returns OK with the k,v written
+    event['written_at'] = str(datetime.now())
+
+    # write notification to SNS topic
+    boto3.client('sns').publish(
+      TargetArn=SNS_ARN,
+      Message=json.dumps(event)
+    )
+
+    # return the event and the code
+    return {
+      'statusCode': 200,
+      'body': json.dumps(event, default=str)
+    }
+  except pymysql.Error as e:
+    print(f"[ERROR] MySQL exception: {e}")
+    exit(-1)
