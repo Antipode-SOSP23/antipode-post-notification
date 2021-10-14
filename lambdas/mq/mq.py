e56c57af036bc7071ddcbc620eb949af5b65c32e
João Ferreira Loff <jfloff@gmail.com>
2021-10-14 16:10:12 +0000

    feat: Retry connect for MQ

--- lambdas/mq/mq.py
+++ lambdas/mq/mq.py
@@ -9,13 +9,19 @@ MQ_NOTIFICATION_QUEUE = os.environ['MQ_NOTIFICATION_QUEUE']
 
 def _conn(role):
   role_region = os.environ[f"{role.upper()}_REGION"]
-  role_host = os.environ[f"MQ_STOMP_HOST__{role_region.replace('-','_').upper()}"]
+  role_host = os.environ[f"MQ_HOST__{role_region.replace('-','_').upper()}"]
 
   host = [(role_host, MQ_PORT)]
   c = stomp.Connection(host)
   c.set_ssl(host)
-  c.connect(MQ_USER, MQ_PASSWORD, wait=False)
-  return c
+
+  while True:
+    try:
+      c.connect(MQ_USER, MQ_PASSWORD, wait=True)
+      return c
+    except stomp.exception.ConnectFailedException as e:
+      print("[ERROR] Failed connecting to MQ. Retrying ...")
+      pass
 
 def write_notification(event):
   c = _conn('writer')
