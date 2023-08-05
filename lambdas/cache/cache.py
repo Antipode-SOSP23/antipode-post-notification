2704e6ba0c9b02268aa3ade6ebb84c0943dfc8ae
João Loff <jfloff@gmail.com>
2023-08-05 11:02:00 +0100

    feat(cache): Update cache implementation

--- lambdas/cache/cache.py
+++ lambdas/cache/cache.py
@@ -1,7 +1,5 @@
 import os
 import redis
-import json
-from datetime import datetime
 
 CACHE_PORT = os.environ['CACHE_PORT']
 
@@ -14,24 +12,14 @@ def _conn(role):
       socket_connect_timeout=5, socket_timeout=5
     )
 
-def write_post(i,k):
+def write_post(k):
   op = (k,) # dont forget the comma :)
-  post = {
-      'i': i,
-      'k': k,
-      'blob': str(os.urandom(1000000))
-    }
-  _conn('writer').set(k, json.dumps(post))
+  _conn('writer').set(k, str(os.urandom(1000000)))
   return op
 
 def read_post(k):
   r = _conn('reader')
   return bool(r.exists(k))
-
-def antipode_shim(id, role):
-  import antipode_cache as ant # this file will get copied when deploying
-
-  return ant.AntipodeCache(_id=id, conn=_conn(role))
 
 def clean():
   # only the writer has permissions to clean
